ebdde5eb09ba72bf786a30515e102a740b6f38be
filipmrkva <filip.mrkva@seznam.cz>
2020-12-02 17:26:21 +0100

    Upraveni domovske stranky

---
+++ application/views/pages/home.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <title>Domovská stránka</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <style>
+    body {
+     background-color: lightsalmon;
+    }
+    </style>
+    </head>
+    <body>
+        <h2 class="text-center"><b><u>Zábavné pokusy na jednom místě</u></b></h2>
+        <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h4><b><u><p style="color:darkblue;">Bezpečnost při zacházení s pokusy:</p></u></b></h4>
+                <h5><b>
+                <p style="border:6px; border-style:outset dashed; border-color:#3366cc; padding: 20px 40px;">
+                    • Při práci s libovolnými pokusy dbáme zvýšené opatrnosti a v případě potřeby dbáme také higieny.<br>
+                    • V případě manipulace s objekty či předměty které mohou poškodit náš zrak nebo být obličeji nebezpečné použijeme <u>bezpečnostní brýle</u>.<br> 
+                    • V případě že manipulujeme s chemikáliemi které mohou reagovat s dotykem a nebo nám ublížit použijeme <u>bezpečnostní rukavice</u>. Vždy po práci si řádně <u>umyjeme ruce</u> (při práci s nebezpečnými předměty nebo chemikáliemi si ruce můžeme mýt častěji i v průběhu je-li potřeba). Při práci s chemikáliemi je vždy <u>nutné dodržovat přesný postup</u> (změna postupu či práce může vést k nečekaným důsledkům, které mohou ohrozit naše zdraví).<br>
+                    • Z hlediska bezpečnosti lze tedy říct, že práce s fyzikálními pokusy je méně nebezpečná než práce s chemickými pokusy, avšak není důvod fyzikální pokusy podceňovat a nedodržovat určité bezpečnostní podmínky.<br>
+                    • Nejčastější chyby a nebezpečí nastává v případě, <u>že si lidé neuvědomují svůj přístup k pokusu</u> a mohou se i omylem dotkou věci pokožce nebezpečné nebo obecně nebezpečné a <u>nebo při nedodržování postupů</u>.<br>
+                    • V případě fyzikálních pokusů by při nedodržení podmínek zřejmě pokus jen nefungoval, ale v případě chemických pokusů by mohl následek chyby být větší a více nebezpečný. <u>Je tedy nutné mýt si pravidelně ruce a ve více nebezpečných případech použít bezpečnostní brýle, rukavice a nebo silnější oblečení</u>.<br>
+                    • Dbáme také na bezpečnost při práci například s ohněm, který bychom neměli zapalovat v blízkosti hořlavých předmětů a vůbec ne v blízkosti (vysoce) hořlavých látek! Oheň zapalujeme nejlépe venku a snažíme se ho mít pod kontrolou, v případě problémů je nutno oheň rychle uhasit, v případě velkých problémů zavolat záchranou složku hasiče.<br> 
+                </p>
+                </b></h5>
+            </div>
+        </div>
+            <div class="row">
+                <div class="col-4">
+                    
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,7 @@
 <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
  
+<div class="text-center">
 <em class="text">Martin Šimčík, Filip Mrkva, David Pospíšek 2020/21</em>
+</div>
         </body>
 </html>
